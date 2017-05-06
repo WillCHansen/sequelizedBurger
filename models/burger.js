cb732ff7999b3460a3a90852dd009f3bc00cf32b
William Hansen <William Hansen>
2017-05-06 01:16:46 -0700

    completed

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,22 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
-};
-// Export the database functions for the controller (catsController.js).
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+// Creates a "Character" model that matches up with DB
+var burger = sequelize.define("burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    }
+  });
+
+// Syncs with DB
+Character.sync();
+
 module.exports = burger;
